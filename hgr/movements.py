7571f2a18e71e5e24fe2388369cd317d4af91a25
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-03-25 23:41:22 +0530

    🌱 Update .task bundle and refactor code

--- hgr/movements.py
+++ hgr/movements.py
@@ -2,13 +2,10 @@ import cv2
 import mediapipe as mp
 from mediapipe.tasks.python import vision
 
-model_path = "tasks/custom_gestures.task"
+model_path = "tasks/gesture_recognizer.task"
 recognizer = vision.GestureRecognizer.create_from_model_path(model_path)
 
 capture = cv2.VideoCapture(0)
-top_gesture = None
-rating = None
-count = 0
 
 while True:
     success, frame = capture.read()
@@ -20,28 +17,22 @@ while True:
     if len(recognition_result.gestures) > 0:
         top_gesture = recognition_result.gestures[0][0]
 
-    if not top_gesture:
-        result = "None"
-        rating = 0.0
-    else:
-        result = top_gesture.category_name
-        rating = top_gesture.score
+        gesture = top_gesture.category_name
+        score = top_gesture.score
 
-    print(f"{count} Gesture recognized: {result} ({rating})")
+        print(f"Gesture recognized: {gesture} ({score})")
 
-    cv2.putText(
-        frame,
-        f"{result}",
-        (0, 40),
-        cv2.FONT_HERSHEY_PLAIN,
-        3.0,
-        (0, 0, 0),
-        3,
-    )
+        cv2.putText(
+            frame,
+            f"{gesture}",
+            (0, 40),
+            cv2.FONT_HERSHEY_PLAIN,
+            3.0,
+            (0, 0, 0),
+            3,
+        )
 
     cv2.imshow("img", frame)
-
-    count += 1
 
     if cv2.waitKey(10) & 0xFF == ord("q"):
         cv2.destroyAllWindows()
