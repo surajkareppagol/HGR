2902c009b28178e91f4e8bb66f8434a9034df1be
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-04-29 19:54:58 +0530

    🌱 Add API class

---
+++ hgr/mp/api.py
@@ -0,0 +1,43 @@
+## Methods
+
+# get_gestures()
+# get_landmarks_image()
+# annotate_image()
+# get_landmark_image()
+# get_gesture_image()
+
+import mp.train
+from mp.gesture import Gesture
+from mp.tracker import Tracker
+
+tracker = Tracker()
+gesture = Gesture()
+
+
+class API:
+    def __init__(self):
+        """Initializes the App class"""
+        pass
+
+    def get_gestures(self):
+        return mp.train.gestures
+
+    def get_landmarks_image(self, image):
+        image = tracker.detect_hands(image)
+        landmarks = tracker.get_landmarks(image)
+        return landmarks
+
+    def annotate_image(self, image):
+        return tracker.detect_hands(image)
+
+    def get_landmark_image(self, image, landmark):
+        image = tracker.detect_hands(image)
+        landmarks = tracker.get_landmarks(image)
+
+        if landmarks:
+            return (landmarks[landmark][1], landmarks[landmark][2])
+        return None
+
+    def get_gesture_image(self, image):
+        gesture_, score = gesture.get_gesture(image)
+        return gesture_
